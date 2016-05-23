921a0ea525a39ec4d0918986b7be8702c40264d5
bhmiller126 <milly_the_mouse@hotmail.com>
2016-05-23 11:28:00 +0100

    customer updated with java names

--- nb-gardens-oaktree/src/main/java/com/qa/oaktree/entities/Customer.java
+++ nb-gardens-oaktree/src/main/java/com/qa/oaktree/entities/Customer.java
@@ -1,130 +1,159 @@
 package com.qa.oaktree.entities;
 
-public class Customer 
-{
-	private String user_name;
+public class Customer {
+	private String userName;
 	private String title;
-	private String first_name;
-	private String last_name;
+	private String firstName;
+	private String lastName;
 	private String password;
-	private String date_of_birth;  
+	private String dateOfBirth;
 	private String email;
-	private String contact_no;
+	private String contactNo;
 	private String status;
-	private String secret_question;
-	private String secret_answer;
-	private String address_line_1;
-	private String address_postcode;
+	private String secretQuestion;
+	private String secretAnswer;
+	private String addressLine1;
+	private String addressPostcode;
 	private boolean gender;
 	private float credit;
-	
-	public Customer(String user_name, String title, String first_name, String last_name, String password, String date_of_birth, String email, String contact_no, String status, String secret_question, String secret_answer, String address_line_1, String address_postcode, boolean gender, float credit)
-	{
-		this.user_name = user_name;
+
+	public Customer(String userName, String title, String firstName, String lastName, String password,
+			String dateOfBirth, String email, String contactNo, String status, String secretQuestion,
+			String secretAnswer, String addressLine1, String addressPostcode, boolean gender, float credit) {
+		this.userName = userName;
 		this.title = title;
-		this.first_name = first_name;
-		this.last_name = last_name;
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.password = password;
-		this.date_of_birth = date_of_birth;
+		this.dateOfBirth = dateOfBirth;
 		this.email = email;
-		this.contact_no = contact_no;
+		this.contactNo = contactNo;
 		this.status = status;
-		this.secret_question = secret_question;
-		this.secret_answer = secret_answer;
-		this.address_line_1 = address_line_1;
-		this.address_postcode = address_postcode;
+		this.secretQuestion = secretQuestion;
+		this.secretAnswer = secretAnswer;
+		this.addressLine1 = addressLine1;
+		this.addressPostcode = addressPostcode;
 		this.gender = gender;
 		this.credit = credit;
 	}
 
-	public String getUser_name() 
-	{return user_name;}
+	public String getUserName() {
+		return userName;
+	}
 
-	public void setUser_name(String user_name) 
-	{this.user_name = user_name;}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-	public String getTitle() 
-	{return title;}
+	public String getTitle() {
+		return title;
+	}
 
-	public void setTitle(String title) 
-	{this.title = title;}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-	public String getFirst_name() 
-	{return first_name;}
+	public String getFirstName() {
+		return firstName;
+	}
 
-	public void setFirst_name(String first_name) 
-	{this.first_name = first_name;}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-	public String getLast_name() 
-	{return last_name;}
+	public String getLastName() {
+		return lastName;
+	}
 
-	public void setLast_name(String last_name) 
-	{this.last_name = last_name;}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-	public String getPassword() 
-	{return password;}
+	public String getPassword() {
+		return password;
+	}
 
-	public void setPassword(String password) 
-	{this.password = password;}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-	public String getDate_of_birth() 
-	{return date_of_birth;}
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
 
-	public void setDate_of_birth(String date_of_birth) 
-	{this.date_of_birth = date_of_birth;}
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
 
-	public String getEmail() 
-	{return email;}
+	public String getEmail() {
+		return email;
+	}
 
-	public void setEmail(String email) 
-	{this.email = email;}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-	public String getContact_no() 
-	{return contact_no;}
+	public String getContactNo() {
+		return contactNo;
+	}
 
-	public void setContact_no(String contact_no) 
-	{this.contact_no = contact_no;}
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
+	}
 
-	public String getStatus() 
-	{return status;}
+	public String getStatus() {
+		return status;
+	}
 
-	public void setStatus(String status) 
-	{this.status = status;}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-	public String getSecret_question() 
-	{return secret_question;}
+	public String getSecretQuestion() {
+		return secretQuestion;
+	}
 
-	public void setSecret_question(String secret_question) 
-	{this.secret_question = secret_question;}
+	public void setSecretQuestion(String secretQuestion) {
+		this.secretQuestion = secretQuestion;
+	}
 
-	public String getSecret_answer() 
-	{return secret_answer;}
+	public String getSecretAnswer() {
+		return secretAnswer;
+	}
 
-	public void setSecret_answer(String secret_answer) 
-	{this.secret_answer = secret_answer;}
+	public void setSecretAnswer(String secretAnswer) {
+		this.secretAnswer = secretAnswer;
+	}
 
-	public String getAddress_line_1() 
-	{return address_line_1;}
+	public String getAddressLine1() {
+		return addressLine1;
+	}
 
-	public void setAddress_line_1(String address_line_1) 
-	{this.address_line_1 = address_line_1;}
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
 
-	public String getAddress_postcode() 
-	{return address_postcode;}
+	public String getAddressPostcode() {
+		return addressPostcode;
+	}
 
-	public void setAddress_postcode(String address_postcode) 
-	{this.address_postcode = address_postcode;}
+	public void setAddressPostcode(String addressPostcode) {
+		this.addressPostcode = addressPostcode;
+	}
 
-	public boolean isGender() 
-	{return gender;}
+	public boolean isGender() {
+		return gender;
+	}
 
-	public void setGender(boolean gender) 
-	{this.gender = gender;}
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
 
-	public float getCredit() 
-	{return credit;}
+	public float getCredit() {
+		return credit;
+	}
 
-	public void setCredit(float credit) 
-	{this.credit = credit;}
-	
+	public void setCredit(float credit) {
+		this.credit = credit;
+	}
 }
