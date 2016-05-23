b69dabdb422aaea7ba7d11d086c4d4ffb5ba52c9
AerosAtar <aerosatar@gmail.com>
2016-05-23 16:39:02 +0100

    Added annotations and comments

--- nb-gardens-oaktree/src/main/java/com/qa/oaktree/entities/Supplier.java
+++ nb-gardens-oaktree/src/main/java/com/qa/oaktree/entities/Supplier.java
@@ -1,79 +1,214 @@
-
 package com.qa.oaktree.entities;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Column; 
 import javax.persistence.Id; 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.ManyToOne; 
+import javax.persistence.OneToOne;
 import javax.persistence.JoinColumn; 
 
+/**
+ * @author Daniel
+ *
+ */
+
+@Entity
+@Table (name = "Supplier")
 public class Supplier {
-	int supplier_Id; 
-	String name; 
-	String contact_No; 
-	String email; 
-	String address_Line_1;
-	String address_Postcode;
 	
-	public Supplier (int supplier_id, String name, String contact_no, String email, String address_line_1, String address_postcode) {
-		
-		this.supplier_Id = supplier_id; 
+	@Id
+	@Column (name = "supplier_id", nullable = false)
+	private int supplierID;
+	
+	@Column (name = "name", nullable = false, length = 45)
+	private String name;
+	
+	@Column (name = "contact_no", nullable = false, length = 13)
+	private String contactPhoneNumber;
+	
+	@Column (name = "email", nullable = true, length = 100)
+	private String contactEmail;
+	
+	@OneToOne
+	@JoinColumn (name = "Address_line_1", nullable = false)
+	private String addressLine1;
+	
+	@OneToOne
+	@JoinColumn (name = "Address_postcode", nullable = false)
+	private String addressPostcode;
+	
+	/**
+	 * Default null constructor for Supplier
+	 */
+	public Supplier()
+	{
+		this.supplierID = -1;
+		this.name = "";
+		this.contactPhoneNumber = "";
+		this.contactEmail = "";
+		this.addressLine1 = "";
+		this.addressPostcode = "";
+	}
+	
+	/**
+	 * Full constructor for Supplier
+	 * @param supplierID; Int autoincrementing integer
+	 * @param name; String Supplier's company name
+	 * @param contactPhoneNumber; String Supplier's contact phone number
+	 * @param contactEmail; String Supplier's contact email address
+	 * @param addressLine1; String First line of supplier's address 
+	 * @param addressPostcode; String Postal code of supplier's address
+	 */
+	public Supplier (int supplierID, String name, String contactPhoneNumber, String contactEmail, String addressLine1, String addressPostcode) 
+	{
+		this.supplierID = supplierID; 
 		this.name = name; 
-		this.contact_No = contact_no; 
-		this.email = email; 
-		this.address_Line_1 = address_line_1; 
-		this.address_Postcode = address_postcode; 
+		this.contactPhoneNumber = contactPhoneNumber; 
+		this.contactEmail = contactEmail; 
+		this.addressLine1 = addressLine1; 
+		this.addressPostcode = addressPostcode; 
 		
 	}
 
-	public int getSupplier_id() {
-		return supplier_Id;
+	/**
+	 * @return the supplierID
+	 */
+	public int getSupplierID() {
+		return supplierID;
 	}
 
-	public void setSupplier_id(int supplier_id) {
-		this.supplier_Id = supplier_id;
-	}
-
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @return the contactPhoneNumber
+	 */
+	public String getContactPhoneNumber() {
+		return contactPhoneNumber;
+	}
+
+	/**
+	 * @return the contactEmail
+	 */
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+	/**
+	 * @return the addressLine1
+	 */
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	/**
+	 * @return the addressPostcode
+	 */
+	public String getAddressPostcode() {
+		return addressPostcode;
+	}
+
+	/**
+	 * @param supplierID the supplierID to set
+	 */
+	public void setSupplierID(int supplierID) {
+		this.supplierID = supplierID;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getContact_no() {
-		return contact_No;
+	/**
+	 * @param contactPhoneNumber the contactPhoneNumber to set
+	 */
+	public void setContactPhoneNumber(String contactPhoneNumber) {
+		this.contactPhoneNumber = contactPhoneNumber;
 	}
 
-	public void setContact_no(String contact_no) {
-		this.contact_No = contact_no;
+	/**
+	 * @param contactEmail the contactEmail to set
+	 */
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
 	}
 
-	public String getEmail() {
-		return email;
+	/**
+	 * @param addressLine1 the addressLine1 to set
+	 */
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	/**
+	 * @param addressPostcode the addressPostcode to set
+	 */
+	public void setAddressPostcode(String addressPostcode) {
+		this.addressPostcode = addressPostcode;
 	}
 
-	public String getAddress_line_1() {
-		return address_Line_1;
+	/**
+	 * Override equals() method for Supplier
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 * @return false if the supplier 'other' is different to this supplier 
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Supplier other = (Supplier) obj;
+		if (addressLine1 == null) {
+			if (other.addressLine1 != null)
+				return false;
+		} else if (!addressLine1.equals(other.addressLine1))
+			return false;
+		if (addressPostcode == null) {
+			if (other.addressPostcode != null)
+				return false;
+		} else if (!addressPostcode.equals(other.addressPostcode))
+			return false;
+		if (contactEmail == null) {
+			if (other.contactEmail != null)
+				return false;
+		} else if (!contactEmail.equals(other.contactEmail))
+			return false;
+		if (contactPhoneNumber == null) {
+			if (other.contactPhoneNumber != null)
+				return false;
+		} else if (!contactPhoneNumber.equals(other.contactPhoneNumber))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (supplierID != other.supplierID)
+			return false;
+		return true;
 	}
 
-	public void setAddress_line_1(String address_line_1) {
-		this.address_Line_1 = address_line_1;
+	/**
+	 * Override object's toString() method for Supplier
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Supplier [supplierID=" + supplierID 
+				+ ", name=" + name 
+				+ ", contactPhoneNumber=" + contactPhoneNumber
+				+ ", contactEmail=" + contactEmail 
+				+ ", addressLine1=" + addressLine1 
+				+ ", addressPostcode=" + addressPostcode + "]";
 	}
-
-	public String getAddress_postcode() {
-		return address_Postcode;
-	}
-
-	public void setAddress_postcode(String address_postcode) {
-		this.address_Postcode = address_postcode;
-	}
-
 }
