46d9648c932cf70698dd3e41101cb8f2cffb8b7f
AerosAtar <aerosatar@gmail.com>
2016-05-24 09:56:28 +0100

    Added annotations and comments

--- nb-gardens-oaktree/src/main/java/com/qa/oaktree/entities/PurchaseOrderEvent.java
+++ nb-gardens-oaktree/src/main/java/com/qa/oaktree/entities/PurchaseOrderEvent.java
@@ -1,41 +1,40 @@
 package com.qa.oaktree.entities;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Date;
+import java.sql.Timestamp;
 import javax.persistence.Column; 
 import javax.persistence.Id; 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.ManyToOne; 
 import javax.persistence.JoinColumn; 
-import javax.print.attribute.DateTimeSyntax;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
 
 @Entity
-@Table (name = "Sales_Order_Event")
+@Table (name = "Purchase_Order_Event")
 public class PurchaseOrderEvent {
 	@Id
-	@Column (name = "sales_event_id" )
+	@Column (name = "purchase_event_id" )
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	public int salesEventId; 
+	private int purchaseEventId; 
 	
 	@ManyToOne
-	@JoinColumn (name = "Sales_Order_sales_id", nullable = false)
-	public int getSalesOrderId() {
-		return salesOrderId;
+	@JoinColumn (name = "Purchase_Order_purchase_id", nullable = false)
+	private int getPurchaseOrderId() {
+		return purchaseOrderId;
 	}
 	@NotNull
-	private int salesOrderId; 
+	private int purchaseOrderId; 
 	
-	//TODO double check on date time stamp
 	@Column (name = "time_stamp", nullable = false)
 	@NotNull
-	private DateTimeSyntax timeStamp;
+	private Timestamp timeStamp;
 	
 	
 	@Column (name = "description", nullable = true, length = 225)
-	@Null
 	@Size (min = 1, max =225)
 	private String description; 
 	
@@ -44,51 +43,147 @@ public class PurchaseOrderEvent {
 	@Size (min = 1, max = 20)
 	private String type; 
 	
-	public PurchaseOrderEvent (int salesEventId, int salesOrderId, DateTimeSyntax timeStamp, String description, String type) {
-		this.salesEventId = salesEventId; 
-		this.salesOrderId = salesOrderId; 
+	/**
+	 * Default null constructor for Purchase Order Event
+	 */
+	public PurchaseOrderEvent()
+	{
+		this.purchaseEventId = -1;
+		this.purchaseOrderId = -1;
+		
+		Date date = new Date();
+		this.timeStamp = new java.sql.Timestamp(date.getTime());
+		
+		this.description = "";
+		this.type = "";
+	}
+	
+	/**
+	 * 
+	 * @param purchaseEventId
+	 * @param purchaseOrderId
+	 * @param timeStamp
+	 * @param description
+	 * @param type
+	 */
+	public PurchaseOrderEvent (int purchaseEventId, int purchaseOrderId, Timestamp timeStamp, String description, String type) {
+		this.purchaseEventId = purchaseEventId; 
+		this.purchaseOrderId = purchaseOrderId; 
 		this.timeStamp = timeStamp; 
 		this.description = description; 
 		this.type = type; 
 		
 	}
 
-	public int getSalesEventId() {
-		return salesEventId;
+	/**
+	 * @return the salesEventId
+	 */
+	public int getPurchaseEventId() {
+		return purchaseEventId;
 	}
 
-	public void setSalesEventId(int salesEventId) {
-		this.salesEventId = salesEventId;
-	}
-
-	
-
-	public void setSalesOrderId(int salesOrderId) {
-		this.salesOrderId = salesOrderId;
-	}
-
-	public DateTimeSyntax getTimeStamp() {
+	/**
+	 * @return the timeStamp
+	 */
+	public Timestamp getTimeStamp() {
 		return timeStamp;
 	}
 
-	public void setTimeStamp(DateTimeSyntax timeStamp) {
-		this.timeStamp = timeStamp;
-	}
-
+	/**
+	 * @return the description
+	 */
 	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
+	/**
+	 * @return the type
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * @param purchaseEventId the purchaseEventId to set
+	 */
+	public void setPurchaseEventId(int purchaseEventId) {
+		this.purchaseEventId = purchaseEventId;
+	}
+
+	/**
+	 * @param purchaseOrderId the purchaseOrderId to set
+	 */
+	public void setPurchaseOrderId(int purchaseOrderId) {
+		this.purchaseOrderId = purchaseOrderId;
+	}
+
+	/**
+	 * @param timeStamp the timeStamp to set
+	 */
+	public void setTimeStamp(Timestamp timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
 
+	/**
+	 * Override method equals() for PurchaseOrderEvent 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 * @return false if the event 'other' is different to this event
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PurchaseOrderEvent other = (PurchaseOrderEvent) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (purchaseEventId != other.purchaseEventId)
+			return false;
+		if (purchaseOrderId != other.purchaseOrderId)
+			return false;
+		if (timeStamp == null) {
+			if (other.timeStamp != null)
+				return false;
+		} else if (!timeStamp.equals(other.timeStamp))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	/**
+	 * Override method toString() for PurchaseOrderEvent
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "PurchaseOrderEvent [purchaseEventId=" + purchaseEventId 
+				+ ", purchaseOrderId=" + purchaseOrderId 
+				+ ", timeStamp=" + timeStamp 
+				+ ", description=" + description 
+				+ ", type=" + type + "]";
+	}
 }
--- nb-gardens-oaktree/src/main/java/com/qa/oaktree/entities/SalesOrderEvent.java
+++ nb-gardens-oaktree/src/main/java/com/qa/oaktree/entities/SalesOrderEvent.java
@@ -1,15 +1,16 @@
 package com.qa.oaktree.entities;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Date;
+import java.sql.Timestamp;
 import javax.persistence.Column; 
 import javax.persistence.Id; 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.ManyToOne; 
 import javax.persistence.JoinColumn; 
-import javax.print.attribute.DateTimeSyntax;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -18,24 +19,22 @@ public class SalesOrderEvent {
 	@Id
 	@Column (name = "sales_event_id" )
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	public int salesEventId; 
+	private int salesEventId; 
 	
 	@ManyToOne
 	@JoinColumn (name = "Sales_Order_sales_id", nullable = false)
-	public int getSalesOrderId() {
+	private int getSalesOrderId() {
 		return salesOrderId;
 	}
 	@NotNull
 	private int salesOrderId; 
 	
-	//TODO double check on date time stamp
 	@Column (name = "time_stamp", nullable = false)
 	@NotNull
-	private DateTimeSyntax timeStamp;
+	private Timestamp timeStamp;
 	
 	
 	@Column (name = "description", nullable = true, length = 225)
-	@Null
 	@Size (min = 1, max =225)
 	private String description; 
 	
@@ -44,7 +43,30 @@ public class SalesOrderEvent {
 	@Size (min = 1, max = 20)
 	private String type; 
 	
-	public SalesOrderEvent (int salesEventId, int salesOrderId, DateTimeSyntax timeStamp, String description, String type) {
+	/**
+	 * Default null constructor for Sales Order Event
+	 */
+	public SalesOrderEvent()
+	{
+		this.salesEventId = -1;
+		this.salesOrderId = -1;
+		
+		Date date = new Date();
+		this.timeStamp = new java.sql.Timestamp(date.getTime());
+		
+		this.description = "";
+		this.type = "";
+	}
+	
+	/**
+	 * 
+	 * @param salesEventId
+	 * @param salesOrderId
+	 * @param timeStamp
+	 * @param description
+	 * @param type
+	 */
+	public SalesOrderEvent (int salesEventId, int salesOrderId, Timestamp timeStamp, String description, String type) {
 		this.salesEventId = salesEventId; 
 		this.salesOrderId = salesOrderId; 
 		this.timeStamp = timeStamp; 
@@ -53,42 +75,115 @@ public class SalesOrderEvent {
 		
 	}
 
+	/**
+	 * @return the salesEventId
+	 */
 	public int getSalesEventId() {
 		return salesEventId;
 	}
 
-	public void setSalesEventId(int salesEventId) {
-		this.salesEventId = salesEventId;
-	}
-
-	
-
-	public void setSalesOrderId(int salesOrderId) {
-		this.salesOrderId = salesOrderId;
-	}
-
-	public DateTimeSyntax getTimeStamp() {
+	/**
+	 * @return the timeStamp
+	 */
+	public Timestamp getTimeStamp() {
 		return timeStamp;
 	}
 
-	public void setTimeStamp(DateTimeSyntax timeStamp) {
-		this.timeStamp = timeStamp;
-	}
-
+	/**
+	 * @return the description
+	 */
 	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
+	/**
+	 * @return the type
+	 */
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * @param salesEventId the salesEventId to set
+	 */
+	public void setSalesEventId(int salesEventId) {
+		this.salesEventId = salesEventId;
+	}
+
+	/**
+	 * @param salesOrderId the salesOrderId to set
+	 */
+	public void setSalesOrderId(int salesOrderId) {
+		this.salesOrderId = salesOrderId;
+	}
+
+	/**
+	 * @param timeStamp the timeStamp to set
+	 */
+	public void setTimeStamp(Timestamp timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
 	public void setType(String type) {
 		this.type = type;
 	}
 
+	/**
+	 * Override method equals() for SalesOrderEvent 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 * @return false if the event 'other' is different to this event
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SalesOrderEvent other = (SalesOrderEvent) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (salesEventId != other.salesEventId)
+			return false;
+		if (salesOrderId != other.salesOrderId)
+			return false;
+		if (timeStamp == null) {
+			if (other.timeStamp != null)
+				return false;
+		} else if (!timeStamp.equals(other.timeStamp))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	/**
+	 * Override method toString() for SalesOrderEvent
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "SalesOrderEvent [salesEventId=" + salesEventId 
+				+ ", salesOrderId=" + salesOrderId 
+				+ ", timeStamp=" + timeStamp 
+				+ ", description=" + description 
+				+ ", type=" + type + "]";
+	}
 }
