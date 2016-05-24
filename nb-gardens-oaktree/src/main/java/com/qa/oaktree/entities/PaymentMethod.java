bdd14cd76697d75005e4d407213e925667b6044d
bhmiller126 <milly_the_mouse@hotmail.com>
2016-05-24 09:40:58 +0100

    payment method complete

--- nb-gardens-oaktree/src/main/java/com/qa/oaktree/entities/PaymentMethod.java
+++ nb-gardens-oaktree/src/main/java/com/qa/oaktree/entities/PaymentMethod.java
@@ -74,7 +74,18 @@ public class PaymentMethod {
 		this.billingAddressPostcode = null; 
 		
 	}
-	
+
+	/**
+	 * Full constructor for payment method
+	 * @param String cardNumber
+	 * @param String cardType
+	 * @param Date startDate
+	 * @param Date expiryDate
+	 * @param String nameOnCard
+	 * @param String billingAddressLine1 from address
+	 * @param String billingAddressPostcode from address
+	 * @param String userName from customer
+	 */
 	
 	
 	public PaymentMethod(String cardNumber, String cardType, Date startDate, Date expiryDate, String nameOnCard,
@@ -90,32 +101,193 @@ public class PaymentMethod {
 		this.billingAddressPostcode = billingAddressPostcode;
 	}
 
+
+/**
+ * get card number method
+ * @return card number of payment method
+ */
 	public String getCardNumber() {
 		return cardNumber;
 	}
-
+	/**
+	 * get card type method
+	 * @return card type of payment method
+	 */
 	public String getCardType() {
 		return cardType;
 	}
-
+	/**
+	 * get start date method
+	 * @return start date of payment method
+	 */
 	public Date getStartDate() {
 		return startDate;
 	}
-
+	/**
+	 * get expiry date method
+	 * @return expiry date of payment method
+	 */
 	public Date getExpiryDate() {
 		return expiryDate;
 	}
-
+	/**
+	 * get name on card method
+	 * @return name on card of payment method
+	 */
 	public String getNameOnCard() {
 		return nameOnCard;
 	}
 
-	public Address getPaymentAddress() {
+	/**
+	 * set username method
+	 * @set the username in customer
+	 */
+	public void setUserName(Customer userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * get billing address line 1 method
+	 * @return the billingAddressLine1
+	 */
+	public com.qa.oaktree.entities.Address getBillingAddressLine1() {
 		return billingAddressLine1;
 	}
 
-	public void setUserName(Customer userName) {
-		this.userName = userName;
+	/**
+	 * set billing addres line 1 method
+	 * @set billingAddressLine1 of address
+	 */
+	public void setBillingAddressLine1(com.qa.oaktree.entities.Address billingAddressLine1) {
+		this.billingAddressLine1 = billingAddressLine1;
+	}
+
+	/**
+	 * gte billing address postcode 
+	 * @return the billingAddressPostcode of address
+	 */
+	public com.qa.oaktree.entities.Address getBillingAddressPostcode() {
+		return billingAddressPostcode;
+	}
+
+	/**
+	 * set billing address method
+	 * @set billingAddressPostcode of address
+	 */
+	public void setBillingAddressPostcode(com.qa.oaktree.entities.Address billingAddressPostcode) {
+		this.billingAddressPostcode = billingAddressPostcode;
+	}
+
+	/**
+	 * set card number method
+	 * @set cardNumber of payment method
+	 */
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	/**
+	 * set name on card method
+	 * @set name on card of payment method
+	 */
+	
+	public void setNameOnCard(String nameOnCard) {
+		this.nameOnCard = nameOnCard;
+	}
+
+	/**
+	 * set cardtype method
+	 * @set cardType of payment method
+	 */
+	public void setCardType(String cardType) {
+		this.cardType = cardType;
+	}
+
+	/**
+	 * set start date method
+	 * @set startDate of payment method
+	 */
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	/**
+	 * set expiry date method
+	 * @set expiryDate of payment method
+	 */
+	public void setExpiryDate(Date expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+	/** 
+	 * Override method for equlas to see if any of the data entered matches a record already in database
+	 * false if payment method other is different to this.PaymentMethod
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PaymentMethod other = (PaymentMethod) obj;
+		if (billingAddressLine1 == null) {
+			if (other.billingAddressLine1 != null)
+				return false;
+		} else if (!billingAddressLine1.equals(other.billingAddressLine1))
+			return false;
+		if (billingAddressPostcode == null) {
+			if (other.billingAddressPostcode != null)
+				return false;
+		} else if (!billingAddressPostcode.equals(other.billingAddressPostcode))
+			return false;
+		if (cardNumber == null) {
+			if (other.cardNumber != null)
+				return false;
+		} else if (!cardNumber.equals(other.cardNumber))
+			return false;
+		if (cardType == null) {
+			if (other.cardType != null)
+				return false;
+		} else if (!cardType.equals(other.cardType))
+			return false;
+		if (expiryDate == null) {
+			if (other.expiryDate != null)
+				return false;
+		} else if (!expiryDate.equals(other.expiryDate))
+			return false;
+		if (nameOnCard == null) {
+			if (other.nameOnCard != null)
+				return false;
+		} else if (!nameOnCard.equals(other.nameOnCard))
+			return false;
+		if (startDate == null) {
+			if (other.startDate != null)
+				return false;
+		} else if (!startDate.equals(other.startDate))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+
+	/** 
+	 * Override to string method, 
+	 */
+	@Override
+	public String toString() {
+		return "PaymentMethod [userName= " + userName + "," 
+				+ "cardNumber=" + cardNumber + "," 
+				+ "nameOnCard= " + nameOnCard + "," 
+				+ "cardType= " + cardType + "," 
+				+ "startDate= " + startDate + "," 
+				+ "expiryDate= " + expiryDate + ","
+				+ "billingAddressLine1= " + billingAddressLine1 + "," 
+				+ "billingAddressPostcode= " + billingAddressPostcode
+				+ "]";
 	}
 
 }
