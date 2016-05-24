9e1f6a35c03dc1068462e227f3c92a06465da1ad
bhmiller126 <milly_the_mouse@hotmail.com>
2016-05-24 10:05:40 +0100

    junit 4 and employee test

--- nb-gardens-oaktree/src/test/java/com/qa/oaktree/EmployeeTest.java
+++ nb-gardens-oaktree/src/test/java/com/qa/oaktree/EmployeeTest.java
@@ -1,5 +1,44 @@
 package com.qa.oaktree;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import com.qa.oaktree.entities.Employee;
+
 public class EmployeeTest {
+	@Test public void testEmployee () {
+		Employee testing = new Employee (); 
+		assertNull(testing.getEmployeeName());  
+	}
+	@Test public void testEmployeeString() {
+		Employee testing = new Employee(); 
+		assertNotNull(testing.getEmployeeDepartment()); 
+	}
+	@Test public void testGetUserName () {
+		Employee testing = new Employee();
+		assertNull(testing.getEmployeeUsername()); 	
+	}
+	@Test public void testGetDepartment() {
+		Employee testing = new Employee(); 
+		assertEquals ("a", testing.getEmployeeDepartment()); 
+	}
+	@Test public void testSetName() {
+		Employee testing = new Employee(); 
+		testing.setEmployeeName("aa");
+		assertEquals("aa", testing.getEmployeeName()); 
+	}
+	@Test public void testSetAccess() {
+		Employee testing = new Employee(); 
+		testing.setEmployeeAccess(0);
+		assertEquals(0, testing.getEmployeeAccess()); 
+	}
+	@Test public void testSetSecretAnswer() {
+		Employee testing = new Employee(); 
+		testing.setEmployeeSecretAnswer(null);
+		assertNull(testing.getEmployeeSecretAnswer());
+	}
 
 }
