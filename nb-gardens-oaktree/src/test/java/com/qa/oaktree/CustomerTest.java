180c11b27587f2815ac5e7706574103891a33c08
AHarris14 <angus.harris@hotmail.com>
2016-05-24 11:25:04 +0100

    CustomerTest finished

--- nb-gardens-oaktree/src/test/java/com/qa/oaktree/CustomerTest.java
+++ nb-gardens-oaktree/src/test/java/com/qa/oaktree/CustomerTest.java
@@ -1,41 +1,46 @@
 package com.qa.oaktree;
 
 import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
 import org.junit.Test;
 import com.qa.oaktree.entities.Customer;
 
 public class CustomerTest {
 	
 	@Test public void testCustomer () {
-		Customer testing = new Customer (null, null, null, null, null, null, null, null, null, null, null, null, null, false, 0); 
-		assertNull(testing.getFirstName());
+		Customer testing = new Customer (); 
+		assertEquals("", testing.getFirstName());
 	}
 	@Test public void testCustomerString() {
-		Customer testing = new Customer(null, null, "a", null, null, null, null, null, null, null, null, null, null, false, 0); 
-		assertNotNull(testing.getFirstName()); 
+		Customer testing = new Customer("", "", "a", "", "", null, true, null, "", "", "", "", "", "", ""); 
+		assertEquals("a", testing.getFirstName()); 
 	}
 	@Test public void testGetUserName () {
-		Customer testing = new Customer(null, null, null, null, null, null, null, null, null, null, null, null, null, false, 0);
+		Customer testing = new Customer("a", "", "", "", "", null, true, null, "", "", "", "", "", "", "");
 		assertNull(testing.getUserName()); 	
 	}
 	@Test public void testGetLastName() {
-		Customer testing = new Customer(null, null, null, "a", null, null, null, null, null, null, null, null, null, false, 0); 
+		Customer testing = new Customer("", "", "", "a", "", null, true, null, "", "", "", "", "", "", ""); 
 		assertEquals ("a", testing.getLastName()); 
 	}
 	@Test public void testSetName() {
-		Customer testing = new Customer(null, null, null, null, null, null, null, null, null, null, null, null, null, false, 0); 
+		Customer testing = new Customer(); 
 		testing.setUserName("aa");
 		assertEquals("aa", testing.getUserName()); 
 	}
 	@Test public void testSetGender() {
-		Customer testing = new Customer(null, null, null, null, null, null, null, null, null, null, null, null, null, false, 0); 
+		Customer testing = new Customer(); 
 		testing.setGender(true);
 		assertEquals(true, testing.isGender()); 
 	}
 	@Test public void testSetCredit() {
-		Customer testing = new Customer(null, null, null, null, null, null, null, null, null, null, null, null, null, false, 0); 
-		testing.setCredit(0);
-		testing.getCredit();
+		Customer testing = new Customer(); 
+		BigDecimal testNum = new BigDecimal("1098.14");
+		testing.setCredit(testNum);
+		assertEquals(testNum, testing.getCredit());
 	}
 }
 
