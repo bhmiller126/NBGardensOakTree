4a481c1a2f286ed61defbf64e2b0aae04db1a183
bhmiller126 <milly_the_mouse@hotmail.com>
2016-05-25 10:23:01 +0100

    payment method test has documentation now

--- nb-gardens-oaktree/src/test/java/com/qa/oaktree/PaymentMethodTest.java
+++ nb-gardens-oaktree/src/test/java/com/qa/oaktree/PaymentMethodTest.java
@@ -6,35 +6,75 @@ import org.junit.Test;
 
 import com.qa.oaktree.entities.PaymentMethod;
 
+/**
+ * 
+ * @author OakTree BM Payment method test
+ *
+ */
 public class PaymentMethodTest {
-	
-	@Test public void testPaymentMethod(){
+
+	/**
+	 * This first test is testing that the object of payment emthod will return
+	 * null but using assertnull
+	 */
+	@Test
+	public void testPaymentMethod() {
 		PaymentMethod testing = new PaymentMethod();
 		assertNull(testing.getUserName());
-		}
-	@Test public void testGetCardNumber(){
+	}
+
+	/**
+	 * This test is testing the getter for card number should return blank as ""
+	 * is thwe value it is checking against to see if its the same
+	 */
+	@Test
+	public void testGetCardNumber() {
 		PaymentMethod testing = new PaymentMethod();
 		assertEquals("", testing.getCardNumber());
-		}
-	@Test public void testSetCardType(){
+	}
+
+	/**
+	 * This test is setting the card type to null, then checking to see if the
+	 * object is not null should return an error as the card type has been set
+	 * to null
+	 */
+	@Test
+	public void testSetCardType() {
 		PaymentMethod testing = new PaymentMethod();
 		testing.setCardType(null);
 		assertNotNull(testing.getCardType());
-		}
-	@Test public void testToString(){
+	}
+
+	/**
+	 * This method is testing the to string method should return an error or
+	 * nothing as there is currently no data for the onject or test to pull
+	 */
+	@Test
+	public void testToString() {
 		PaymentMethod testing = new PaymentMethod();
-		testing.toString(); 
-		}
-	@Test public void testSetExpiryDate(){
+		testing.toString();
+	}
+
+	/**
+	 * This test is testing the set expiry method, the expiry date is set to
+	 * null should return that it is null as assert null is used
+	 */
+	@Test
+	public void testSetExpiryDate() {
 		PaymentMethod testing = new PaymentMethod();
 		testing.setExpiryDate(null);
 		assertNull(testing.getExpiryDate());
-		}
-	@Test public void testSetBillingAddresss(){
+	}
+
+	/**
+	 * This tests the set nilling address Should return two nulls, as billing
+	 * address line 1 and billing address postcode have both been set to null
+	 */
+	@Test
+	public void testSetBillingAddresss() {
 		PaymentMethod testing = new PaymentMethod();
 		testing.setBillingAddressLine1(null);
 		testing.setBillingAddressPostcode(null);
 		assertNull(testing.getBillingAddressLine1(), testing.getBillingAddressPostcode());
-		}
 	}
-
+}
