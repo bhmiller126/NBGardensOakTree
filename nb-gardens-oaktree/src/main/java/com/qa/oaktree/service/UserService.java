51912c5a69bea103b927d8c91df8dcda68cb270f
Barry <barry_martin13@hotmail.com>
2016-06-01 16:56:56 +0100

    service package

---
+++ nb-gardens-oaktree/src/main/java/com/qa/oaktree/service/UserService.java
@@ -0,0 +1,30 @@
+package com.qa.oaktree.service;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import com.qa.oaktree.repositories.CustomerRepository;
+
+/**
+ * This stateless bean hold business logic associated with the customer entity
+ * 
+ * @author Barry
+ *
+ */
+@Stateless
+public class UserService
+{
+	@Inject private CustomerRepository customerRepository;
+	
+	public boolean validateCredentials(String username, String password)
+	{
+		if(customerRepository.findByUsernameAndPassword(username, password) == null)
+			return false;
+		return true;
+	}
+	
+	public long getUserID(String username)
+	{
+		return customerRepository.findByUsername(username).getId();
+	}
+}
