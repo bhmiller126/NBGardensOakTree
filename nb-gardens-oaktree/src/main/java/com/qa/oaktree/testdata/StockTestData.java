91e1b62b6cef649c2744af26e5efc0f17f232b17
bhmiller126 <milly_the_mouse@hotmail.com>
2016-06-03 15:26:55 +0100

    stock test data edit

--- nb-gardens-oaktree/src/main/java/com/qa/oaktree/testdata/StockTestData.java
+++ nb-gardens-oaktree/src/main/java/com/qa/oaktree/testdata/StockTestData.java
@@ -1,56 +1,72 @@
 package com.qa.oaktree.testdata;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
-import com.qa.oaktree.entities.Stock;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Singleton;
 
+import org.junit.Test;
+
+import com.qa.oaktree.entities.Stock;
+
 /**
  * 
- * @author OakTree BM
- *	Stock Test
+ * @author OakTree BM Product Test
  */
-@Singleton	
+@Singleton
 public class StockTestData {
-	
-/**
+	/**
 	 * This hold our test data for testing that the application runs properly
 	 * 
 	 * 
 	 */
-	
+
 	private List<Stock> stock;
 
-		public void stockTestData() {
-			stock = new ArrayList<Stock>();
-			stock.add(new Stock (0, new BigDecimal(""), 1, 1, 1, "back order", "G4", 1));
-		}
-		
-		public  List<Stock> getStock() {
-			return stock;
-		}
-	
-	
-	
-	@Test public void testGetCatalogueId() {
-		Stock testing = new Stock(); 
+	public void productTestData() {
+		stock = new ArrayList<Stock>();
+		stock.add(new Stock(1, new BigDecimal(" "), 1, 1, 1, " back order", "G3", 2));
+	}
+
+	public List<Stock> getStock() {
+		return stock;
+	}
+
+	@Test
+	public void testGetCatalogueId() {
+		Stock testing = new Stock();
 		assertNull(testing.getCatalogueId());
 	}
-	@Test public void testGetStockStatus(){
+
+	@Test
+	public void testGetStockStatus() {
 		Stock testing = new Stock();
 		assertNull(testing.getStockStatus());
-		}
-	
-	@Test public void testSetReorderLevel(){
+	}
+
+	@Test
+	public void testSetStockStatus() {
 		Stock testing = new Stock();
-		testing.setReorderLevel(0);
-		assertEquals(0, testing.getReorderLevel());
-		}
+		testing.setStockStatus(" ");
+		assertEquals(" ", testing.getStockStatus());
+	}
 	
+	@Test
+	public void testSetWarehouseLocation(){
+		Stock testing = new Stock(); 
+		testing.setWarehouseLocation("n");
+		assertEquals ("n", testing.getWarehouseLocation());
+	}
 	
+	@Test
+	public void testGetWarehouseLocation() {
+		Stock testing = new Stock(); 
+		assertNull(testing.getWarehouseLocation());
+	}
+	@Test
 }
 
 
