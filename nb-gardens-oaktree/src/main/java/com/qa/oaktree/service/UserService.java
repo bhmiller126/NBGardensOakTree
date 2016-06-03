5646332a3c84d5c9be31ad4bc7f4274cc563ea73
AHarris14 <angus.harris@hotmail.com>
2016-06-03 14:04:01 +0100

    added hash methods to user service for use with user passwords

--- nb-gardens-oaktree/src/main/java/com/qa/oaktree/service/UserService.java
+++ nb-gardens-oaktree/src/main/java/com/qa/oaktree/service/UserService.java
@@ -1,5 +1,9 @@
 package com.qa.oaktree.service;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -27,5 +31,39 @@ public class UserService
 	public String getUserID(String username)
 	{
 		return customerRepository.findByUsername(username).getUserName();
+	}
+	
+	
+	/**
+	 * Hash method that takes a string message and returns the string representation of the
+	 * resulting hash using the SHA-256 algorithm
+	 * @param aString the string to hash
+	 * @return the hashed value
+	 */
+	private String hashString(String aString){
+		try {
+	        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+	        byte[] hashedBytes = digest.digest(aString.getBytes("UTF-8"));
+	 
+	        return convertByteArrayToHexString(hashedBytes);
+	    } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "";
+		}
+	}
+	
+	/**
+	 * Converts a byte array into a hex string to make it human readable
+	 * @param arrayBytes the byte array from the hashing algorithm
+	 * @return a hex string of the byte array
+	 */
+	private static String convertByteArrayToHexString(byte[] arrayBytes) {
+	    StringBuffer stringBuffer = new StringBuffer();
+	    for (int i = 0; i < arrayBytes.length; i++) {
+	        stringBuffer.append(Integer.toString((arrayBytes[i] & 0xff) + 0x100, 16)
+	                .substring(1));
+	    }
+	    return stringBuffer.toString();
 	}
 }
